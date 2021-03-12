bce9d6728fcff4b72c19948071dadfec09f3b27e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-12 16:16:56 -0300

    Rename file

--- Simple.DatabaseWrapper/TypeReader/TypeInfo.cs
+++ Simple.DatabaseWrapper/TypeReader/TypeInfo.cs
@@ -11,6 +11,12 @@ namespace Simple.DatabaseWrapper.TypeReader
         public string TypeName { get; set; }
         public TypeItemInfo[] Items { get; set; }
 
+        public IEnumerable<string> NamesOf(ColumnAttributes attribute)
+        {
+            return Items.Where(o => o.Is(attribute)).Select(i => i.Name);
+        }
+        public string FirstNameOf(ColumnAttributes attribute) => NamesOf(attribute).FirstOrDefault();
+
         public static TypeInfo FromType<T>() => FromType(typeof(T));
         public static TypeInfo FromType(Type type)
         {
