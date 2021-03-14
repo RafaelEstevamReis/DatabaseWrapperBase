3395424f126ac8cff0187db249f494c61b770ebc
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-14 12:45:56 -0300

    NuGet Package

--- Simple.DatabaseWrapper/TypeReader/TypeInfo.cs
+++ Simple.DatabaseWrapper/TypeReader/TypeInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Simple.DatabaseWrapper.Attributes;
 
 namespace Simple.DatabaseWrapper.TypeReader
 {
