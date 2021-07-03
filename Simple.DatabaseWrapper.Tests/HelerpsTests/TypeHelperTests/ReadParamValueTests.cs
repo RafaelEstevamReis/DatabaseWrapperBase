14876fb7a95c38463f30039d616f0a3dedd7f364
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-07-03 13:47:32 -0300

    Add Tests

--- Simple.DatabaseWrapper.Tests/HelerpsTests/TypeHelperTests/ReadParamValueTests.cs
+++ Simple.DatabaseWrapper.Tests/HelerpsTests/TypeHelperTests/ReadParamValueTests.cs
@@ -1,5 +1,7 @@
 ﻿using Simple.DatabaseWrapper.Helpers;
 using Simple.DatabaseWrapper.TypeReader;
+using System;
+using System.Drawing;
 using Xunit;
 
 namespace Simple.DatabaseWrapper.Tests.HelerpsTests.TypeHelperTests
@@ -29,6 +31,43 @@ namespace Simple.DatabaseWrapper.Tests.HelerpsTests.TypeHelperTests
                 if (!p.CanRead) continue;
                 _ = TypeHelper.ReadParamValue(p, testObject);
             }
+        }
+
+        [Fact]
+        public void ReadParamValue_SpecialFields_Timespan()
+        {
+            var testObject = new Special();
+            var typeInfo = TypeInfo.FromType<Special>();
+
+            testObject.Span = TimeSpan.Zero;
+            Assert.Equal((Int64)0, TypeHelper.ReadParamValue(typeInfo.Items[0], testObject));
+
+            testObject.Span = TimeSpan.FromTicks(21);
+            Assert.Equal((Int64)21, TypeHelper.ReadParamValue(typeInfo.Items[0], testObject));
+
+            testObject.Span = TimeSpan.FromHours(2);
+            Assert.Equal((Int64)72000000000, TypeHelper.ReadParamValue(typeInfo.Items[0], testObject));
+        }
+        [Fact]
+        public void ReadParamValue_SpecialFields_Color()
+        {
+            var testObject = new Special();
+            var typeInfo = TypeInfo.FromType<Special>();
+
+            testObject.Color = Color.White;
+            Assert.Equal(new byte[] { 255, 255, 255, 255 }, TypeHelper.ReadParamValue(typeInfo.Items[1], testObject));
+
+            testObject.Color = Color.Black;
+            Assert.Equal(new byte[] { 255, 0, 0, 0 }, TypeHelper.ReadParamValue(typeInfo.Items[1], testObject));
+
+            testObject.Color = Color.FromArgb(10, 20, 30, 40);
+            Assert.Equal(new byte[] { 10, 20, 30, 40 }, TypeHelper.ReadParamValue(typeInfo.Items[1], testObject));
+        }
+
+        class Special
+        {
+            public TimeSpan Span { get; set; }
+            public Color Color { get; set; }
         }
     }
 }
