4a76b8c21fe67bab5780b2208f468a80752cbd87
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-08-03 00:22:18 -0300

    Add GetColumnByName

--- Simple.DatabaseWrapper/Interfaces/ITable.cs
+++ Simple.DatabaseWrapper/Interfaces/ITable.cs
@@ -1,9 +1,16 @@
-﻿namespace Simple.DatabaseWrapper.Interfaces
+﻿using System.Linq;
+
+namespace Simple.DatabaseWrapper.Interfaces
 {
     public interface ITable
     {
         string TableName { get; }
         IColumn[] Columns { get; }
+
+#if !NET20_OR_GREATER && !NETSTANDARD
+        IColumn GetColumnByName(string name) 
+            => Columns.First(c => string.Equals(c.ColumnName, name, System.StringComparison.InvariantCultureIgnoreCase));
+#endif
 
         string ExportCreateTable();
     }
