25ff772559b55ffe33c9ddafe80d46bbd0b86aa6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-11-07 00:26:03 -0300

    Read Guid as byte[] in ReadParamValue

--- Simple.DatabaseWrapper/Helpers/TypeHelper.cs
+++ Simple.DatabaseWrapper/Helpers/TypeHelper.cs
@@ -40,10 +40,9 @@ namespace Simple.DatabaseWrapper.Helpers
         public static object ReadParamValue(TypeItemInfo info, object parameters)
         {
             var objVal = info.GetValue(parameters);
-            if (objVal is Color color)
-            {
-                return new byte[] { color.A, color.R, color.G, color.B };
-            }
+            if (objVal is Guid guid) return guid.ToByteArray();
+            if (objVal is Color color) return new byte[] { color.A, color.R, color.G, color.B };
+
             if (objVal is TimeSpan span)
             {
                 objVal = span.Ticks;
