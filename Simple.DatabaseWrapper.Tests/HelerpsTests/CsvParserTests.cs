938d49591e3cfd58c09f4b570760e95da8c30362
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-09-03 13:08:00 -0300

    Add CsvParser

---
+++ Simple.DatabaseWrapper.Tests/HelerpsTests/CsvParserTests.cs
@@ -0,0 +1,34 @@
+﻿using Simple.DatabaseWrapper.Helpers;
+using System.Linq;
+using Xunit;
+
+namespace Simple.DatabaseWrapper.Tests.HelerpsTests
+{
+    public class CsvParserTests
+    {
+        [Fact]
+        public void Parse_EmptyString_ReturnsEmpty()
+        {
+            var result = CsvParser.ParseCsvString(string.Empty);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+        [Fact]
+        public void Parse_Complex()
+        {
+            string content = "Name,Age,Description\r\nJohn,30,\"Software Engineer, Senior\"\r\n,,\r\nJane,25,\"Data Scientist\r\nwith multiple projects\"";
+            var result = CsvParser.ParseCsvString(content).ToArray();
+            Assert.NotNull(result);
+
+            Assert.Equal(4, result.Length);
+            // Row 1
+            Assert.Equal(4, result[1][0].Length);
+            Assert.Contains(",", result[1][2]);
+            // Row 3
+            Assert.Equal(0, result[2][2].Length);
+            // Row 4
+            Assert.Contains("\n", result[3][2]);
+        }
+    }
+}
--- Simple.DatabaseWrapper/Helpers/Parsers.cs
+++ Simple.DatabaseWrapper/Helpers/Parsers.cs
@@ -1,7 +1,9 @@
 ﻿namespace Simple.DatabaseWrapper.Helpers;
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 public static class ConnectionStringParser
@@ -86,5 +88,137 @@ public static class ConnectionStringParser
         }
 
         return result;
+    }
+}
+public static class CsvParser
+{
+    /// <summary>
+    /// Parses a CSV file into an Enumerable (rows) of string[] (columns)
+    /// </summary>
+    /// <param name="filePath">File to parse</param>
+    /// <param name="encoding">File encoding to use</param>
+    /// <param name="quote">Quote char to use</param>
+    /// <param name="delimiter">Delimiter char to use</param>
+    public static IEnumerable<string[]> ParseCsvFile(string filePath, Encoding? encoding = null, char quote = '"', char delimiter = ',')
+    {
+        using var sr = new StreamReader(filePath, encoding ?? Encoding.UTF8);
+        return ParseCsv(sr, quote, delimiter);
+    }
+    /// <summary>
+    /// Parses a CSV file into an Enumerable (rows) of string[] (columns)
+    /// </summary>
+    /// <param name="content">String content to parse</param>
+    /// <param name="quote">Quote char to use</param>
+    /// <param name="delimiter">Delimiter char to use</param>
+    public static IEnumerable<string[]> ParseCsvString(string content, char quote = '"', char delimiter = ',')
+    {
+        if (string.IsNullOrEmpty(content)) yield break;
+
+        byte[] bytes = Encoding.UTF8.GetBytes(content);
+        using MemoryStream stream = new(bytes);
+        using StreamReader reader = new(stream, Encoding.UTF8);
+
+        foreach (var row in ParseCsv(reader, quote, delimiter))
+        {
+            yield return row;
+        }
+    }
+    /// <summary>
+    /// Parses a CSV file into an Enumerable (rows) of string[] (columns)
+    /// </summary>
+    /// <param name="reader">Reader to get rows from</param>
+    /// <param name="quote">Quote char to use</param>
+    /// <param name="delimiter">Delimiter char to use</param>
+    public static IEnumerable<string[]> ParseCsv(StreamReader reader, char quote, char delimiter)
+    {
+        bool inQuote = false;
+        List<string> fields = [];
+        StringBuilder fieldBuilder = new StringBuilder();
+
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+
+                if (inQuote)
+                {
+                    if (c == quote)
+                    {
+                        // Verifica se é uma aspa escapada ("" dentro de campo quotado)
+                        if (i + 1 < line.Length && line[i + 1] == quote)
+                        {
+                            fieldBuilder.Append(quote);
+                            i++; // Pula a próxima aspa
+                        }
+                        else
+                        {
+                            inQuote = false; // Sai do modo quotado
+                        }
+                    }
+                    else
+                    {
+                        fieldBuilder.Append(c);
+                    }
+                }
+                else
+                {
+                    if (c == quote)
+                    {
+                        inQuote = true; // Entra no modo quotado
+                    }
+                    else if (c == delimiter)
+                    {
+                        fields.Add(fieldBuilder.ToString());
+#if NET20
+                        fieldBuilder = new StringBuilder();
+#else
+                        fieldBuilder.Clear();
+#endif
+                    }
+                    else
+                    {
+                        fieldBuilder.Append(c);
+                    }
+                }
+            }
+
+            // Se estamos em um campo quotado, adiciona a quebra de linha
+            if (inQuote)
+            {
+                fieldBuilder.Append('\n');
+            }
+            else
+            {
+                // Fim da linha, adiciona o último campo e retorna a linha
+                fields.Add(fieldBuilder.ToString());
+#if NET20
+                fieldBuilder = new StringBuilder();
+#else
+                fieldBuilder.Clear();
+#endif
+                yield return fields.ToArray();
+                fields.Clear();
+            }
+        }
+
+        // Processa o último campo, se houver
+        if (fieldBuilder.Length > 0 || fields.Count > 0)
+        {
+            fields.Add(fieldBuilder.ToString());
+            yield return fields.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Converts CSV rows to T instance
+    /// </summary>
+    /// <typeparam name="T">Target Type</typeparam>
+    /// <param name="rows">Parsed CSV Rows</param>
+    /// <param name="converter">Mapper function</param>
+    public static IEnumerable<T> As<T>(this IEnumerable<string[]> rows, Func<string[], T> converter)
+    {
+        foreach(var row in rows) yield return converter(row);
     }
 }
