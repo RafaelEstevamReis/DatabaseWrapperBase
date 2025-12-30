4283dd22927816e1e6590279db9e0a048deb455e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-12-30 19:01:05 -0300

    Add EnumPolicyAttribute

---
+++ Simple.DatabaseWrapper/Attributes/EnumPolicyAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Simple.DatabaseWrapper.Attributes
+{
+    /// <summary>
+    /// Specify enum handling policy
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EnumPolicyAttribute : Attribute
+    {
+        /// <summary>
+        /// Available policies
+        /// </summary>
+        public enum Policies
+        {
+            /// <summary>
+            /// Should be stored as number
+            /// </summary>
+            AsNumber = 0,
+            /// <summary>
+            /// Should be stored as text
+            /// </summary>
+            AsText = 1,
+        }
+        /// <summary>
+        /// Current Policy
+        /// </summary>
+        public Policies Policy { get; set; }
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="policy">Policy to be used</param>
+        public EnumPolicyAttribute(Policies policy)
+        {
+            Policy = policy;
+        }
+    }
+}
