a67a5f78b5548adfa1a61072235841695cf34ccc
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-12-30 22:53:26 -0300

    Deprecate ConfigureTable and add EditTable to IColumnMapper

--- Simple.DatabaseWrapper/Interfaces/IColumnMapper.cs
+++ Simple.DatabaseWrapper/Interfaces/IColumnMapper.cs
@@ -4,6 +4,8 @@ namespace Simple.DatabaseWrapper.Interfaces
 {
     public interface IColumnMapper : ITableMapper
     {
+        [Obsolete($"Use {nameof(EditTable)} instead")]
         ITableMapper ConfigureTable(Action<ITable> Options);
+        ITableMapper EditTable(Action<ITable> Options);
     }
 }
