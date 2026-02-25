f107e46ed076c0e9a34f95d001b81f617365d095
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-02-25 15:54:57 -0300

    Nuget Package

---
+++ Simple.DatabaseWrapper/Helpers/ObsoleteClasses.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple.DatabaseWrapper.Helpers;
+
+using System;
+
+[Obsolete("Use Simple.DatabaseWrapper.Helpers.Parsers namespace", error: true)]
+public static class CsvParser
+{
+}
+
+[Obsolete("Use Simple.DatabaseWrapper.Helpers.Parsers namespace", error: true)]
+public static class ConnectionStringParser
+{
+
+}
